41606483906524544f3f6b7b60d87d7f3baa044a
Loongsai <loongsai@gmail.com>
2018-10-18 09:11:45 +0700

    update for nov newsletter

--- www/define-array-newsletter.php
+++ www/define-array-newsletter.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 
-// array in each set is (month , newsletter name, link online, type,powered) 
+// array in each set is (month , newsletter name, link online, type,powered)
 // type 1 = IZ newsl  2= EDM  3= Greeting card
 
 $set2015=array(
@@ -9,7 +9,7 @@ $set2015=array(
 	array(2,'IMAGE asia Newsletter','https://www.image-asia.com/newsletters/2015/february/index.htm',1,1),
 	array(3,'IMAGE asia Newsletter','https://www.image-asia.com/newsletters/2015/march/index.htm',1,1),
 	array(4,'IMAGE asia Newsletter','https://www.image-asia.com/newsletters/2015/april/index.htm',1,1),
-	array(5,'IMAGE asia Newsletter','https://www.image-asia.com/newsletters/2015/may/index.htm',1,1),	
+	array(5,'IMAGE asia Newsletter','https://www.image-asia.com/newsletters/2015/may/index.htm',1,1),
 	array(6,'IMAGE asia Newsletter','https://www.image-asia.com/newsletters/2015/june/index.htm',1,1),
 	array(7,'IMAGE asia Newsletter','https://www.image-asia.com/newsletters/2015/july/index.htm',1,1),
 	array(7,'Special Introductory Offer on the e-newsletter Everything Phuket by IMAGE asia','https://www.image-asia.com/newsletters/2015/july/introduce.htm',2,1),
@@ -72,7 +72,7 @@ $set2017=array(
 	array(8,'Everything Phuket in August by IMAGE asia','https://www.image-asia.com/newsletters/2017/august/index.htm',1,1),
 	array(8,'Training for Revenue Management in Thailand for Hoteliers. This Month! - HSMAI','https://www.image-asia.com/newsletters/2017/august/hsmai.htm',2,1),
 	array(9,'Where to Eat in Phuket September-October issue is out now','https://www.image-asia.com/newsletters/2017/september/wte005.htm',1,1),
-	array(9,'Window on Phuket September-October issue is out now','https://www.image-asia.com/newsletters/2017/september/win005.htm',1,1),	
+	array(9,'Window on Phuket September-October issue is out now','https://www.image-asia.com/newsletters/2017/september/win005.htm',1,1),
 	array(10,'Everything Phuket in October by IMAGE asia','https://www.image-asia.com/newsletters/2017/october/index.htm',1,1),
 	array(11,'WINDOW on Phuket November-December issue is out now','https://www.image-asia.com/newsletters/2017/november/win006.htm',1,1),
 	array(11,'Where to Eat in Phuket November-December issue is out now','https://www.image-asia.com/newsletters/2017/november/wte006.htm',1,1),
@@ -80,7 +80,7 @@ $set2017=array(
 	array(12,'Kata Rocks cements the KRSR as a world-class yachting event','https://www.image-asia.com/newsletters/2017/december/kata-rocks-superyacht-rendezvous-2017.htm',2,1),
 	array(12,'Merry Christmas & Happy Year from IMAGE asia','https://www.image-asia.com/newsletters/2017/december/happy-new-year-2018.htm',3,1),
 	array(12,'Don\'t miss Thailand’s definitive marine and luxury lifestyle showcase.','https://www.image-asia.com/newsletters/2017/december/the-phuket-rendezvous.htm',2,1),
-	
+
 );
 
 
@@ -89,21 +89,32 @@ $set2018=array(
 	array(1,'WINDOW on Phuket January-February issue is out now','https://www.image-asia.com/newsletters/2018/january/wop-is-out-issue111.htm',1,1),
 	array(2,'Everything Phuket in February by IMAGE asia','https://www.image-asia.com/newsletters/2018/february/index.htm',1,1),
 	array(2,'Your invitation to Join the Thailand Yacht Show 2018','https://www.image-asia.com/newsletters/2018/february/tys-dedicated-edm.htm',2,1),
-	array(3,'Where to Eat in Phuket March-April issue is out now','https://mailchi.mp/728c0e751ff4/where-to-eat-in-phuket-march-april-issue-is-out-now',1,2),	
+	array(3,'Where to Eat in Phuket March-April issue is out now','https://mailchi.mp/728c0e751ff4/where-to-eat-in-phuket-march-april-issue-is-out-now',1,2),
 	array(3,'WINDOW on Phuket March-April issue is out now','https://mailchi.mp/a1e494517b30/window-on-phuket-march-april-issue-is-out-now',1,2),
 	array(4,'Everything Phuket in April by IMAGE asia','https://mailchi.mp/2f6022e81f56/everything-phuket-in-april-by-image-asia',1,2),
 	array(5,'Where to Eat in Phuket May-June issue is out now','https://mailchi.mp/f6861f414f62/where-to-eat-in-phuket-may-june-issue-is-out-now',1,2),
 	array(5,'WINDOW on Phuket May-June issue is out now','https://mailchi.mp/03c15b081dce/a42tg42eo3',1,2),
 	array(6,'Everything Phuket in June by IMAGE asia','https://mailchi.mp/1e365d950c78/everything-phuket-in-june-by-image-asia',1,2),
-	
-		
+
+
 	array(7,'15th Anniversary Cape Panwa Hotel Phuket Raceweek ready to set sail','https://mailchi.mp/7bc048cf0f42/15th-anniversary-cape-panwa-hotel-phuket-raceweek-ready-to-set-sail',2,2),
 	array(7,'Where to Eat in Phuket July-August issue is out now','https://mailchi.mp/6104ea746dbc/where-to-eat-in-phuket-july-august-issue-is-out-now',1,2),
 	array(7,'WINDOW on Phuket July-August issue is out now','https://mailchi.mp/ef752fe0a835/where-to-eat-in-phuket-july-august-issue-is-out-now-296441',1,2),
-	
+
 	array(8,'Everything Phuket in August by IMAGE asia','https://mailchi.mp/7247a4ba382c/everything-phuket-in-august-by-image-asia?e=c0e4b389e3',1,2),
-	
-	
+
+	array(9,'Where to Eat in Phuket September-October issue is out now','https://mailchi.mp/83c3b8866728/where-to-eat-in-phuket-september-october-issue-is-out-now',1,2),
+
+	array(9,'WINDOW on Phuket September-October issue is out now','https://mailchi.mp/1dc7bd9120a1/window-on-phuket-september-october-issue-is-out-now',1,2),
+
+	array(9,'PHUKET BRIEFING & BUSINESS NETWORKING (BCCT)','https://mailchi.mp/38bdc46dacc1/ud3yj2m4n2?e=c0e4b389e3',2,2),
+
+	array(10,'Everything Phuket in October by IMAGE asia','https://mailchi.mp/3e9ec336db4b/everything-phuket-in-october-by-image-asia?e=c0e4b389e3',1,2),
+
+	array(11,'Where to Eat in Phuket November-December issue is out now','',1,2),
+	array(11,'WINDOW on Phuket November-December issue is out now','',1,2),
+
+
 );
 
  ?>
